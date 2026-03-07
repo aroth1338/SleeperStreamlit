25309cafa3a2ba8a417292498841fb930eec226b
Adam Roth <81182782+aroth1338@users.noreply.github.com>
2026-03-07 15:38:42 -0500

    formatting changes

--- main.py
+++ main.py
@@ -1,75 +1,11 @@
 import streamlit as st
 import httpx
 from datetime import date
-
-@st.dialog(title = "Login", width = "medium", dismissible=False,)
-def get_sleeper_username():
-
-    st.html(
-        """
-        <style>
-            div[data-testid="stHeading"] {
-                padding: 0 rem;
-            }
-        </style>
-        """
-    )
-    years = [date.today().year - i for i in range(5)]
-
-    st.subheader("Sleeper Username")
-    username = st.text_input(
-        label = "",
-        placeholder = "Username",
-        label_visibility="hidden",
-        key = "get_username"
-    )
-
-    st.subheader("League Year")
-    st.selectbox(
-        label = "",
-        options = years,
-        key = "year_select"
-    )
-
-    with st.columns(5)[-1]:
-        fetch_league = st.button(
-            label = "Fetch Leagues",
-            type = "primary",
-            key = "fetch_league_data",
-            width = "stretch"
-        )
-    
-    if fetch_league:
-        sleeper_client = httpx.Client(
-            base_url="https://api.sleeper.app/v1/user/"
-        )
-
-        user_info = sleeper_client.get(
-            f"{username}"
-        )
-
-        if user_info.is_error:
-            st.error("Username not recognized by Sleeper.")
-        else:
-            #save the info
-            st.session_state.sleeper_client = sleeper_client
-            st.session_state.user_info = user_info.json()
-            st.session_state.user_id = st.session_state.user_info['user_id']
-
-            league_data = st.session_state.sleeper_client.get(
-                f"{st.session_state.user_id}/leagues/nfl/2025"
-            ).json()
-            st.session_state.leagues = {
-                x['name']: x for x in league_data
-            }
-
-            st.session_state._initialized = True
-            st.rerun()
-
+from utils.sleeper import get_sleeper_leagues
 
 
 if "_initialized" not in st.session_state:
-    get_sleeper_username()
+    get_sleeper_leagues()
     st.stop()
 
 st.write(st.session_state.user_info)
--- utils/sleeper.py
+++ utils/sleeper.py
@@ -1,24 +1,80 @@
-import requests 
+import streamlit as st
+import httpx 
+from datetime import date
 
-def check_response(ruvesponse, message = None):
-    if message == None:
-        message = "Request failed."
+@st.dialog(title = "Login", width = "medium", dismissible=False,)
+def get_sleeper_leagues():
+    """
+    Initial dialog window to get username and select a league year.
+    """
+    
+    st.html(
+        """
+        <style>
+            h3 {
+                margin-top: 0;
+                margin-bottom: 0;
+                padding-top: 1rem;
+            }
 
-    if response.status_code != 200:
-        raise RuntimeError(f"{message} Error code: {response.status_code}")
+            label[data-testid="stWidgetLabel"] {
+                font-size: 0;
+                min-height: 0;
+            }
 
-if "sleeper_players" not in locals():
-    response = requests.get("https://api.sleeper.app/v1/players/nfl")
-    check_response(response, "Failed to fetch player data.")
-    sleeper_players = response.json()
+        </style>
+        """
+    )
+    years = [date.today().year - i for i in range(5)]
 
-    username = "ARoth13"
+    st.html("<h3>Sleeper Username</h3>")
+    username = st.text_input(
+        label = "",
+        placeholder = "Username",
+        label_visibility="hidden",
+        key = "get_username"
+    )
 
-response = requests.get(f"https://api.sleeper.app/v1/user/{username}")
-check_response(response, "Failed to fetch user data.")
-user_id = response.json()["user_id"]
+    st.html("<h3>League Year</h3>")
+    st.selectbox(
+        label = "",
+        options = years,
+        key = "year_select",
+        label_visibility="hidden"
+    )
 
+    with st.columns(5)[-1]:
+        fetch_league = st.button(
+            label = "Fetch Leagues",
+            type = "primary",
+            key = "fetch_league_data",
+            width = "stretch"
+        )
+    
+    if fetch_league:
+        sleeper_client = httpx.Client(
+            base_url="https://api.sleeper.app/v1/user/"
+        )
 
-response = requests.get(f"https://api.sleeper.app/v1/user/{user_id}/leagues/nfl/2023")
-check_response(response, "Failed to fetch league data.")
-league_json = response.json()
+        user_info = sleeper_client.get(
+            f"{username}"
+        )
+
+        if user_info.is_error:
+            st.error("Username not recognized by Sleeper.")
+        else:
+            #save the info
+            st.session_state.sleeper_client = sleeper_client
+            st.session_state.user_info = user_info.json()
+            st.session_state.user_id = st.session_state.user_info['user_id']
+
+            league_data = st.session_state.sleeper_client.get(
+                f"{st.session_state.user_id}/leagues/nfl/2025"
+            ).json()
+
+            st.session_state.leagues = {
+                x['name']: x for x in league_data
+            }
+
+            st.session_state._initialized = True
+            st.rerun()
